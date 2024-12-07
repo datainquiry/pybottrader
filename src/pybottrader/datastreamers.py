27e03697354b108866dacf6fb1f3d3acb594edf3
Jaime Lopez <jailop@protonmail.com>
2024-12-06 19:52:29 -0500

    Update

--- src/pybottrader/datastreamers.py
+++ src/pybottrader/datastreamers.py
@@ -3,6 +3,7 @@
 from typing import Union
 from attrs import define
 import pandas as pd
+import yfinance
 from .strategies import Position
 
 
@@ -27,6 +28,31 @@ class DataStreamer:
     def next(self) -> Union[dict, None]:
         """Next method"""
 
+
+class YFinanceStreamer(DataStreamer):
+    """Using Yahoo Finance to retrieve data"""
+    index = 0
+    data: pd.DataFrame
+    def __init__(self, symbol, *args, **kwargs):
+        super().__init__()
+        ticker = yfinance.Ticker(symbol)
+        self.data = ticker.history(*args, **kwargs)
+        self.data.rename(columns={
+            "Open": "open",
+            "Close": "close",
+            "High": "high",
+            "Low": "low",
+            "Volume": "volume",
+        }, inplace = True)
+        self.data.index.names = ["time"]
+        self.data.reset_index(inplace=True)
+        
+    def next(self) -> Union[dict, None]:
+        if self.index >= len(self.data):
+            return None
+        result = self.data.iloc[self.index].to_dict()
+        self.index += 1
+        return result
 
 class CSVFileStreamer(DataStreamer):
     """
